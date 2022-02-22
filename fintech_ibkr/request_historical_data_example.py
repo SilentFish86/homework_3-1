6eb05e99888d2b9d78b750e8369d0729e36e70d9
ry86 <yanruijin15@gmail.com>
2022-02-22 18:42:16 -0500

    hw3

--- fintech_ibkr/request_historical_data_example.py
+++ fintech_ibkr/request_historical_data_example.py
@@ -4,22 +4,90 @@
 #  synchronous_functions.py, this script should return a dataframe that's
 #  ready to be loaded into the candlestick graph constructor.
 
+# from fintech_ibkr import *
+import pandas as pd
+from ibapi.client import EClient
+from ibapi.wrapper import EWrapper
 from ibapi.contract import Contract
-from fintech_ibkr import *
+import threading
+import time
+import plotly.graph_objects as go
+
+# def req_historical_data(contract, endDateTime='', durationStr='30 D',
+#                        barSizeSetting='1 hour', whatToShow='MIDPOINT',
+#                        useRTH=True):
+
+class ibkr_app(EWrapper, EClient):
+    def __init__(self):
+        EClient.__init__(self, self)
+        self.error_messages = pd.DataFrame(columns=[
+            'reqId', 'errorCode', 'errorString'
+        ])
+        self.next_valid_id = None
+        self.historical_data = pd.DataFrame(columns=["date", "open", "high", "low", "close"])
+        self.historical_data_end = ''  # pd.DataFrame()
+
+    def error(self, reqId, errorCode, errorString):
+        print("Error: ", reqId, " ", errorCode, " ", errorString)
+
+    def nextValidId(self, orderId: int):
+        self.next_valid_id = orderId
+
+    def historicalData(self, reqId, bar):
+        # YOUR CODE GOES HERE: Turn "bar" into a pandas dataframe, formatted
+        #   so that it's accepted by the plotly candlestick function.
+        # Take a look at candlestick_plot.ipynb for some help!
+        # assign the dataframe to self.historical_data.
+        row = pd.DataFrame(
+            {'date': [bar.date],
+             'open': [bar.open],
+             'high': [bar.high],
+             'low': [bar.low],
+             'close': [bar.close]
+             }
+        )
+        self.historical_data = pd.concat([self.historical_data, row], ignore_index=True)
+
+    def historicalDataEnd(self, reqId: int, start: str, end: str):
+        # super().historicalDataEnd(reqId, start, end)
+        print("HistoricalDataEnd. ReqId:", reqId, "from", start, "to", end)
+        self.historical_data_end = reqId
+
+app = ibkr_app()
+
+app.connect('127.0.0.1', 7497, 10645)
+while not app.isConnected():
+    time.sleep(0.01)
+
+def run_loop():
+    app.run()
+
+# Start the socket in a thread
+api_thread = threading.Thread(target=run_loop, daemon=True)
+api_thread.start()
+
+while isinstance(app.next_valid_id, type(None)):
+    time.sleep(0.01)
 
 value = "EUR.USD" # This is what your text input looks like on your app
-
 # Create a contract object
 contract = Contract()
 contract.symbol = value.split(".")[0]
-contract.secType  = 'CASH'
+contract.secType = 'CASH'
 contract.exchange = 'IDEALPRO'  # 'IDEALPRO' is the currency exchange.
 contract.currency = value.split(".")[1]
 
-# Get your historical data
-historical_data = fetch_historical_data(contract)
 
-# Print it! This should be a dataframe that's ready to go.
-print(historical_data)
+tickerId = app.next_valid_id
+app.reqHistoricalData(
+    tickerId, contract, endDateTime='',           # <-- make a reactive input
+    durationStr='30 D',       # <-- make a reactive input
+    barSizeSetting='1 hour',  # <-- make a reactive input
+    whatToShow='MIDPOINT',    # <-- make a reactive input
+    useRTH= 0,                 # <-- make a reactive input
+    formatDate=1, keepUpToDate=False, chartOptions=[])
 
-# This script is an excellent place for scratch work as you figure this out.
+while app.historical_data_end != tickerId:
+    time.sleep(0.01)
+
+app.historical_data
--- fintech_ibkr/synchronous_functions.py
+++ fintech_ibkr/synchronous_functions.py
@@ -1,4 +1,3 @@
-
 import pandas as pd
 from ibapi.client import EClient
 from ibapi.wrapper import EWrapper
@@ -8,7 +7,8 @@ import time
 # If you want different default values, configure it here.
 default_hostname = '127.0.0.1'
 default_port = 7497
-default_client_id = 10645 # can set and use your Master Client ID
+default_client_id = 10645  # can set and use your Master Client ID
+
 
 # This is the main app that we'll be using for sync and async functions.
 class ibkr_app(EWrapper, EClient):
@@ -27,7 +27,7 @@ class ibkr_app(EWrapper, EClient):
         # I've already done the same general process you need to go through
         # in the self.error_messages instance variable, so you can use that as
         # a guide.
-        self.historical_data = ''
+        self.historical_data = pd.DataFrame(columns=["date", "open", "high", "low", "close"])
         self.historical_data_end = ''
         self.contract_details = ''
         self.contract_details_end = ''
@@ -53,12 +53,22 @@ class ibkr_app(EWrapper, EClient):
         # Take a look at candlestick_plot.ipynb for some help!
         # assign the dataframe to self.historical_data.
         # print(reqId, bar)
-        self.historical_data = bar
+        row = pd.DataFrame(
+            {'date': [bar.date],
+             'open': [bar.open],
+             'high': [bar.high],
+             'low': [bar.low],
+             'close': [bar.close]
+             }
+        )
+        self.historical_data = pd.concat([self.historical_data, row], ignore_index=True)
+        # self.historical_data = bar
 
     def historicalDataEnd(self, reqId: int, start: str, end: str):
         # super().historicalDataEnd(reqId, start, end)
         print("HistoricalDataEnd. ReqId:", reqId, "from", start, "to", end)
         self.historical_data_end = reqId
+
 
 def fetch_managed_accounts(hostname=default_hostname, port=default_port,
                            client_id=default_client_id):
@@ -66,8 +76,10 @@ def fetch_managed_accounts(hostname=default_hostname, port=default_port,
     app.connect(hostname, port, client_id)
     while not app.isConnected():
         time.sleep(0.01)
+
     def run_loop():
         app.run()
+
     api_thread = threading.Thread(target=run_loop, daemon=True)
     api_thread.start()
     while isinstance(app.next_valid_id, type(None)):
@@ -75,56 +87,19 @@ def fetch_managed_accounts(hostname=default_hostname, port=default_port,
     app.disconnect()
     return app.managed_accounts
 
-<<<<<<< HEAD
 
-def req_historical_data(contract, endDateTime='', durationStr='30 D',
-                        barSizeSetting='1 hour', whatToShow='MIDPOINT',
-                        useRTH=True):
-
-    class ibkr_app(EWrapper, EClient):
-        def __init__(self):
-            EClient.__init__(self, self)
-            self.error_messages = pd.DataFrame(columns=[
-                'reqId', 'errorCode', 'errorString'
-            ])
-            self.next_valid_id = None
-            self.historical_data = ''  # pd.DataFrame()
-            self.historical_data_end = ''  # pd.DataFrame()
-
-        def error(self, reqId, errorCode, errorString):
-            print("Error: ", reqId, " ", errorCode, " ", errorString)
-
-        def nextValidId(self, orderId: int):
-            self.next_valid_id = orderId
-
-        def historicalData(self, reqId, bar):
-            split
-            self.historical_data=pd.dataframe();
-            pd.concat
-            # YOUR CODE GOES HERE: Turn "bar" into a pandas dataframe, formatted
-            #   so that it's accepted by the plotly candlestick function.
-            # Take a look at candlestick_plot.ipynb for some help!
-            # assign the dataframe to self.historical_data.
-            print(reqId, bar)
-            self.historical_data = bar
-
-        def historicalDataEnd(self, reqId: int, start: str, end: str):
-            # super().historicalDataEnd(reqId, start, end)
-            print("HistoricalDataEnd. ReqId:", reqId, "from", start, "to", end)
-            self.historical_data_end = reqId
-
-=======
 def fetch_historical_data(contract, endDateTime='', durationStr='30 D',
                           barSizeSetting='1 hour', whatToShow='MIDPOINT',
                           useRTH=True, hostname=default_hostname,
                           port=default_port, client_id=default_client_id):
->>>>>>> ef988fa13e06fce25543c24bb0fdf2785ea2c18c
     app = ibkr_app()
     app.connect(hostname, port, client_id)
     while not app.isConnected():
         time.sleep(0.01)
+
     def run_loop():
         app.run()
+
     api_thread = threading.Thread(target=run_loop, daemon=True)
     api_thread.start()
     while isinstance(app.next_valid_id, type(None)):
