5797e5e2b74eb162d6458436d15ff656d4b6be41
unknown <vcm@vcm-24401.win.duke.edu>
2022-03-29 18:05:49 -0400

    hw3 final

--- fintech_ibkr/synchronous_functions.py
+++ fintech_ibkr/synchronous_functions.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 import pandas as pd
 from ibapi.client import EClient
 from ibapi.wrapper import EWrapper
@@ -8,6 +10,7 @@ import time
 default_hostname = '127.0.0.1'
 default_port = 4002
 default_client_id = 10645  # can set and use your Master Client ID
+timeout_sec = 5
 
 
 # This is the main app that we'll be using for sync and async functions.
@@ -18,6 +21,7 @@ class ibkr_app(EWrapper, EClient):
             'reqId', 'errorCode', 'errorString'
         ])
         self.next_valid_id = None
+        self.current_time = None
         ########################################################################
         # Here, you'll need to change Line 30 to initialize
         # self.historical_data as a dataframe having the column names you
@@ -31,6 +35,11 @@ class ibkr_app(EWrapper, EClient):
         self.historical_data_end = ''
         self.contract_details = ''
         self.contract_details_end = ''
+        self.order_status = pd.DataFrame(
+            columns=['orderId', 'status', 'filled', 'remaining', 'avgFillPrice',
+                     'permId', 'parentId', 'lastFillPrice', 'clientId',
+                     'whyHeld', 'mktCapPrice']
+        )
 
     def error(self, reqId, errorCode, errorString):
         print("Error: ", reqId, " ", errorCode, " ", errorString)
@@ -46,6 +55,9 @@ class ibkr_app(EWrapper, EClient):
 
     def nextValidId(self, orderId: int):
         self.next_valid_id = orderId
+
+    def currentTime(self, time: int):
+        self.current_time = datetime.fromtimestamp(time)
 
     def historicalData(self, reqId, bar):
         # YOUR CODE GOES HERE: Turn "bar" into a pandas dataframe, formatted
@@ -69,14 +81,64 @@ class ibkr_app(EWrapper, EClient):
         print("HistoricalDataEnd. ReqId:", reqId, "from", start, "to", end)
         self.historical_data_end = reqId
 
-    def contractDetails(self, reqId:int, contractDetails):
+    def contractDetails(self, reqId: int, contractDetails):
         print(type(contractDetails))
         print(contractDetails)
         self.contract_details = contractDetails
 
-    def contractDetailsEnd(self, reqId:int):
+    def contractDetailsEnd(self, reqId: int):
         print("ContractDetailsEnd. ReqId:", reqId)
         self.contract_details_end = reqId
+
+    def orderStatus(self, orderId, status: str, filled: float,
+                    remaining: float, avgFillPrice: float, permId: int,
+                    parentId: int, lastFillPrice: float, clientId: int,
+                    whyHeld: str, mktCapPrice: float):
+        print('order status')
+        # print('orderId:' + str(orderId))
+        # print('status:' + status)
+        # print('filled:' + str(filled))
+        # print('remaining:' + str(remaining))
+        # print('avgFillPrice:' + str(avgFillPrice))
+        # print('permId:' + str(permId))
+        # print('parentId:' + str(parentId))
+        # print('lastFillPrice:' + str(lastFillPrice))
+        # print('clientId:' + str(clientId))
+        # print('whyHeld:' + str(69))
+        # print('mktCapPrice:' + str(mktCapPrice))
+
+        print(self.order_status)
+        print(type(self.order_status))
+        self.order_status = pd.concat(
+            [
+                self.order_status,
+                pd.DataFrame({
+                    'order_id': [orderId],
+                    'status': [status],
+                    'filled': [filled],
+                    'remaining': [remaining],
+                    'avg_fill_price': [avgFillPrice],
+                    'perm_id': [permId],
+                    'parent_id': [parentId],
+                    'last_fill_price': [lastFillPrice],
+                    'client_id': [clientId],
+                    'why_held': [whyHeld],
+                    'mkt_cap_price': [mktCapPrice],
+                    'timestamp': ['']
+                })
+            ],
+            ignore_index=True
+        )
+        self.order_status.drop_duplicates(inplace=True)
+
+    def openOrder(self, orderId, contract, order, orderState):
+        print('open order')
+        print(contract)
+        print(order)
+        print(orderState)
+
+    def openOrderEnd(self):
+        print('open order end')
 
 
 def fetch_managed_accounts(hostname=default_hostname, port=default_port,
@@ -124,7 +186,7 @@ def fetch_historical_data(contract, endDateTime='', durationStr='30 D',
 
 
 def fetch_contract_details(contract, hostname=default_hostname,
-                          port=default_port, client_id=default_client_id):
+                           port=default_port, client_id=default_client_id):
     app = ibkr_app()
     app.connect(hostname, port, client_id)
     while not app.isConnected():
@@ -148,3 +210,120 @@ def fetch_contract_details(contract, hostname=default_hostname,
 
     app.disconnect()
     return app.contract_details
+
+
+def place_order(contract, order, hostname=default_hostname,
+                port=default_port, client_id=default_client_id):
+    app = ibkr_app()
+    app.connect(hostname, port, client_id)
+    while not app.isConnected():
+        time.sleep(0.01)
+
+    def run_loop():
+        app.run()
+
+    api_thread = threading.Thread(target=run_loop, daemon=True)
+    api_thread.start()
+
+    while app.next_valid_id is None:
+        time.sleep(0.01)
+
+    app.placeOrder(app.next_valid_id, contract, order)
+    while not (('Submitted' in set(app.order_status['status']) or ('Filled' in set(app.order_status['status'])))):
+        time.sleep(0.25)
+
+    app.disconnect()
+
+    return app.order_status
+
+
+def fetch_contract_details_new(contract, hostname=default_hostname,
+                               port=default_port, client_id=default_client_id):
+    app = ibkr_app()
+    app.connect(hostname, int(port), int(client_id))
+    start_time = datetime.now()
+    while not app.isConnected():
+        time.sleep(0.01)
+        if (datetime.now() - start_time).seconds > timeout_sec:
+            app.disconnect()
+            raise Exception(
+                "fetch_contract_details",
+                "timeout",
+                "couldn't connect to IBKR"
+            )
+
+    def run_loop():
+        app.run()
+
+    api_thread = threading.Thread(target=run_loop, daemon=True)
+    api_thread.start()
+    start_time = datetime.now()
+    while app.next_valid_id is None:
+        time.sleep(0.01)
+        if (datetime.now() - start_time).seconds > timeout_sec:
+            app.disconnect()
+            raise Exception(
+                "fetch_contract_details",
+                "timeout",
+                "next_valid_id not received"
+            )
+
+    tickerId = app.next_valid_id
+    app.reqContractDetails(tickerId, contract)
+
+    start_time = datetime.now()
+    while app.contract_details_end != tickerId:
+        time.sleep(0.01)
+        if (datetime.now() - start_time).seconds > timeout_sec:
+            app.disconnect()
+            raise Exception(
+                "fetch_contract_details",
+                "timeout",
+                "contract_details not received"
+            )
+
+    app.disconnect()
+
+    return app.contract_details
+
+def fetch_current_time(hostname=default_hostname,
+                       port=default_port, client_id=default_client_id):
+    app = ibkr_app()
+    app.connect(hostname, port, client_id)
+    start_time = datetime.now()
+    while not app.isConnected():
+        time.sleep(0.01)
+        if (datetime.now() - start_time).seconds > timeout_sec:
+            raise Exception(
+                "fetch_current_time",
+                "timeout",
+                "couldn't connect to IBKR"
+            )
+
+    def run_loop():
+        app.run()
+
+    api_thread = threading.Thread(target=run_loop, daemon=True)
+    api_thread.start()
+    start_time = datetime.now()
+    while isinstance(app.next_valid_id, type(None)):
+        time.sleep(0.01)
+        if (datetime.now() - start_time).seconds > timeout_sec:
+            raise Exception(
+                "fetch_current_time",
+                "timeout",
+                "next_valid_id not received"
+            )
+
+    app.reqCurrentTime()
+    start_time = datetime.now()
+    while isinstance(app.current_time, type(None)):
+        time.sleep(0.01)
+        if (datetime.now() - start_time).seconds > timeout_sec:
+            raise Exception(
+                "fetch_current_time",
+                "timeout",
+                "current_time not received"
+            )
+    app.disconnect()
+    return app.current_time
